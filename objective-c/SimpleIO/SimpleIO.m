bc1c2de9a3942dd9307ab5bc7a9913f6cedb0c78
August Mueller <gus@flyingmeat.com>
2017-07-19 10:49:37 -0700

    Updated example showing how to load a custom file with deep image support.

--- objective-c/SimpleIO/SimpleIO.m
+++ objective-c/SimpleIO/SimpleIO.m
@@ -42,9 +42,14 @@
     return YES;
 }
 
+
 - (BOOL)readImageForDocument:(id<ACDocument>)document fromURL:(NSURL *)absoluteURL ofType:(NSString *)type error:(NSError **)outError {
     
     NSImage *i = [[NSImage alloc] initWithContentsOfURL:absoluteURL];
+    
+    if (!i) {
+        return [self readUnknownImageForDocument:document fromURL:absoluteURL ofType:type error:outError];
+    }
     
     NSPICTImageRep *r = (NSPICTImageRep*)[[i representations] firstObject];
     
@@ -65,6 +70,38 @@
     NSString *fileName = [[[absoluteURL path] lastPathComponent] stringByDeletingPathExtension];
     
     [baseGroup insertCGImage:imageRef atIndex:0 withName:fileName];
+    
+    return YES;
+}
+
+- (BOOL)readUnknownImageForDocument:(id<ACDocument>)document fromURL:(NSURL *)absoluteURL ofType:(NSString *)type error:(NSError **)outError {
+    
+    // OK, this might not be a pict file! However, it might be something else that we're testing out deep image support for. Like… say a 16 bit tiff with the extension .pict.
+    // So let's let imageio handle this, and see what happens.
+    // Really, this whole method is to show how to load up and create a document for an image source with greater than 8bpc.
+    
+    CGImageSourceRef imageSourceRef = CGImageSourceCreateWithData((__bridge CFDataRef)[NSData dataWithContentsOfURL:absoluteURL], (__bridge CFDictionaryRef)[NSDictionary dictionary]);
+    CGImageRef img                  = CGImageSourceCreateImageAtIndex(imageSourceRef, 0, (__bridge CFDictionaryRef)[NSDictionary dictionary]);
+    CFRelease(imageSourceRef);
+    
+    if (!img) {
+        return NO;
+    }
+    
+    [document setCanvasSize:NSMakeSize(CGImageGetWidth(img), CGImageGetHeight(img))];
+    
+    [document setBitsPerPixel:CGImageGetBitsPerComponent(img) * 4];
+    
+    id <ACGroupLayer> baseGroup = [document baseGroup];
+    
+    NSString *fileName = [[[absoluteURL path] lastPathComponent] stringByDeletingPathExtension];
+    
+    [baseGroup insertCGImage:img atIndex:0 withName:fileName];
+    
+    [(NSDocument *)document setFileURL:nil];
+    [(NSDocument *)document setFileType:nil];
+    
+    CGImageRelease(img);
     
     return YES;
 }
